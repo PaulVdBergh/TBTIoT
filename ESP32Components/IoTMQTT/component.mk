6718afa7cc34a3adc6b5f4a8af945e346d97923e
Paul Van den Bergh <VdBergh.Paul@gmail.com>
2018-03-09 11:36:07 +0100

    cleanup 11h35

--- ESP32Components/IoTMQTT/component.mk
+++ ESP32Components/IoTMQTT/component.mk
@@ -1,2 +1,2 @@
-COMPONENT_SRCDIRS=src
-COMPONENT_PRIV_INCLUDEDIRS=src
+COMPONENT_SRCDIRS=src MQTTClient-C/src MQTTPacket/src MQTTClient-C/src/linux ESP32_Sample
+COMPONENT_PRIV_INCLUDEDIRS=src MQTTClient-C/src MQTTPacket/src  MQTTClient-C/src/linux
